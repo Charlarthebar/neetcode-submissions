7a05abfdcc28011d179bd97e7c9e5951769bf9ad
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-05-24 16:38:02 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,42 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+    def addWord(self, word: str) -> None:
+        if not word:
+            self.isWord = True
+            return
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = WordDictionary()
+            cur = cur.children[c]
+        cur.isWord = True
+
+    def search(self, word: str) -> bool:
+        if not word:
+            return self.isWord
+        c = word[0]
+        if c == ".":
+            for child in self.children.values():
+                if child.search(word[1:]):
+                    return True
+        if c not in self.children:
+            return False
+        return self.children[c].search(word[1:])
+
+
+        # # if not word:
+        #     return self.isWord
+        # cur = self
+        # for i, c in enumerate(word):
+        #     if c == ".":
+        #         for anyc in cur.children.values():
+        #             if anyc.search(word[i+1:]):
+        #                 return True
+        #     if c not in cur.children:
+        #         return False
+        #     cur = cur.children[c]
+        # return self.isWord
