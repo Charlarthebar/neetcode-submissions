77840131d4a8eb9cf4fb1c4bda7f10c975f7d4af
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-05-25 00:40:39 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        old_new = {}
+
+        def dfs(old):
+            # make a clone if it doesn't already exist, 
+            # recursively add neighbors, and return clone
+            if old in old_new:
+                return old_new[old]
+            
+            new = Node(old.val)
+            old_new[old] = new
+
+            for neighbor in old.neighbors:
+                new.neighbors.append(dfs(neighbor))
+            
+            return new
+        return dfs(node)
+
