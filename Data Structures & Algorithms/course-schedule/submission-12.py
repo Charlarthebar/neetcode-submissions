5d9c16532f6769b97d775facb1402e807a525d7d
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-05-25 20:09:50 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = {i: set() for i in range(numCourses)}
+        for a, b in prerequisites:
+            prereqs[a].add(b)
+        print(prereqs)
+
+        visited = set()
+        
+        def cycle_check(cur):
+            if cur in visited:
+                return True
+            if not prereqs[cur]:
+                return False
+
+            visited.add(cur)
+            res = False
+            for course in prereqs[cur]:
+                res = res or cycle_check(course)
+            visited.remove(cur)
+            return res
+
+        for k, v in prereqs.items():
+            for val in v:
+                print(k, val)
+                if cycle_check(val):
+                    return False
+        return True
