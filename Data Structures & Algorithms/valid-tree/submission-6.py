cef0ce4ba0a347ec3cd926d5a61f026b596efb59
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-05-25 20:50:57 -0400

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges:
+            return True
+
+        tree = {i: [] for i in range(n)}
+
+        for a, b in edges:
+            tree[a].append(b)
+            tree[b].append(a)
+
+        print(tree)
+        visited = set()
+
+        def has_cycle(node, prev):
+            if node in visited:
+                return True
+            if not tree[node]:
+                return False
+            
+            visited.add(node)
+            for neighbor in tree[node]:
+                if neighbor == prev:
+                    continue
+                if has_cycle(neighbor, node):
+                    return True
+            return False
+        
+        if has_cycle(0, -1):
+            print(visited)
+            return False
+        print("here")
+        if len(visited) == n:
+            return True
+        return False
+        
