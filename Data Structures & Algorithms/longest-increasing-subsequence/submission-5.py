0f20f7d721b672d38f69c1256cdcea6e80d6d57b
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-05-29 14:45:03 -0400

    Add: longest-increasing-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        maxToCounts = {num: -1 for num in nums}
+        res = 1
+
+        for num in nums:
+            for m, count in maxToCounts.items():
+                if (m != num and # make sure not itself
+                count != -1 and # make sure have passed through it
+                num > m and # make sure new number is greater
+                maxToCounts[m] + 1 > maxToCounts[num]): # number starting at sequence n is greater than current length
+                    print(num, m, count)
+                    maxToCounts[num] = maxToCounts[m] + 1
+                    res = max(res, maxToCounts[num])
+            if maxToCounts[num] == -1:
+                maxToCounts[num] = 1 
+        print(maxToCounts)
+        return res
