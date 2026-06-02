2e7c8522e8b7cae713481c6a47199c625ddf0547
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-06-02 17:34:22 -0400

    Add: rotate-matrix - submission-5

---
+++ Data Structures & Algorithms/rotate-matrix/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        def rotate_corner(r, c):
+            top_right = matrix[c][n-1-r]
+            matrix[c][n-1-r] = matrix[r][c]
+
+            bottom_right = matrix[n-1-r][n-1-c]
+            matrix[n-1-r][n-1-c] = top_right
+
+            bottom_left = matrix[n-1-c][r]
+            matrix[n-1-c][r] = bottom_right
+
+            matrix[r][c] = bottom_left
+
+        dim = n
+        for i in range(n // 2):
+            for j in range(dim - 1):
+                rotate_corner(i, i + j)
+                # print(i, j)
+                # for row in matrix:
+                #     print(row)
+                # print()
+            dim -= 2
