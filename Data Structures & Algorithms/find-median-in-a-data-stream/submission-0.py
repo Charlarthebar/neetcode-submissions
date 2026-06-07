9a5e0797b6521eb36e8f34067fe2f21656024852
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-06-06 22:26:24 -0400

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.py
@@ -0,0 +1,31 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.order = []
+
+    def addNum(self, num: int) -> None:
+        if not self.order:
+            self.order.append(num)
+            return
+        insert = 0
+        for i in range(len(self.order)):
+            if self.order[i] == num:
+                insert = i + 1
+                self.order.insert(insert, num)
+                return
+            elif num > self.order[i]:
+                insert = i + 1
+            else:
+                self.order.insert(insert, num)
+                return
+        self.order.insert(insert, num)
+
+    def findMedian(self) -> float:
+        if len(self.order) % 2 == 1:
+            return self.order[int(len(self.order) / 2)]
+        else:
+            smaller = self.order[int(len(self.order) / 2 - 1)]
+            larger = self.order[int(len(self.order) / 2)]
+            print(self.order)
+            return (smaller + larger) / 2
+        
