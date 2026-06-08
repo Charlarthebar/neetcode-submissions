010b572fc00636863c54ef858a318dbed88f5dbc
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-06-08 14:07:37 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freqsToWords = {}
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            s = tuple(count)
+            if s in freqsToWords:
+                freqsToWords[s].append(word)
+            else:
+                freqsToWords[s] = [word]
+        res = []
+        for l in freqsToWords.values():
+            res.append(l)
+        return res
+    
