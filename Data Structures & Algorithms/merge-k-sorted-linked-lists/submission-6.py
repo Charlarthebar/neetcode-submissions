80e5097c99ba2ebb12773d6aa844f8daa941b316
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-07-11 17:00:14 -0400

    Add: merge-k-sorted-linked-lists - submission-6

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-6.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        def merge(l1, l2):
+            dummy = ListNode()
+            cur = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    cur.next = l1
+                    l1 = l1.next
+                else:
+                    cur.next = l2
+                    l2 = l2.next
+                cur = cur.next
+            cur.next = l1 if l1 else l2
+            return dummy.next
+
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged.append(merge(l1, l2))
+            lists = merged
+        return lists[0]
+
+
+
+        for l in lists:
+            prev = dummy
+            cur = prev.next
+
+            while l:
+                c1 = not cur or l.val <= cur.val
+                c2 = l.val >= prev.val
+                if c1 and c2:
+                    prev.next = l
+                    nxt = l.next
+                    l.next = cur
+                    l = nxt
+                    prev = prev.next
+                else:
+                    prev = cur
+                    cur = cur.next
+        return dummy.next
