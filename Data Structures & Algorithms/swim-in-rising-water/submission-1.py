c2ee7404705d6b9d87845e49c7a6510f5f88d291
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-07-29 13:46:44 -0400

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        def genNeighbors(r, c):
+            neighbors = []
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < n:
+                    neighbors.append((nr, nc))
+            return neighbors
+
+        heap = [(grid[0][0], (0, 0))] # grid value, coord, max value in path
+        visited = {(0, 0)}
+        while heap:
+            time, coord = heapq.heappop(heap)
+            r, c = coord
+            if r == n - 1 and c == n - 1:
+                return time
+            for nr, nc in genNeighbors(r, c):
+                if (nr, nc) not in visited:
+                    heapq.heappush(heap, (max(time, grid[nr][nc]), (nr, nc)))
+                    visited.add((nr, nc))
+        
