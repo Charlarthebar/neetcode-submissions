65047fdcb57b455bca6ad36d6a26d803902b5362
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-07-30 13:21:22 -0400

    Add: foreign-dictionary - submission-33

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-33.py
@@ -0,0 +1,45 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adjList = defaultdict(set)
+        allLetters = set()
+
+        # for w1, w2 in zip(words[:-1], words[1:]):
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return ""
+                if w1[j] != w2[j]:
+                    adjList[w1[j]].add(w2[j])
+                    break
+        for word in words:
+            for ch in word:
+                allLetters.add(ch)
+
+        print(adjList)
+        print(allLetters)
+        res = []
+        visited = set()
+        def dfs(node, path):
+            if node in path:
+                return False
+            
+            if node in visited:
+                return True
+            path.add(node)
+            
+            for neighbor in adjList[node]:
+                if not dfs(neighbor, path):
+                    return False
+            res.append(node)
+            path.remove(node)
+            visited.add(node)
+            return True
+        
+        for ch in allLetters:
+            if not dfs(ch, set()):
+                print(ch)
+                return ""
+            print(ch, res)
+        return "".join(res[::-1])
+        
