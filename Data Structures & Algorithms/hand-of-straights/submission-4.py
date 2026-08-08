56ad190297a73aa6ea0735a4f1b12e8eb2739a13
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-08 17:39:14 -0400

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        print(len(hand))
+        minVal, maxVal = float('inf'), float('-inf')
+        counts = defaultdict(int)
+        for val in hand:
+            counts[val] += 1
+            minVal = min(val, minVal)
+            maxVal = max(val, maxVal)
+        print(counts)
+        for n in range(minVal, maxVal + 1):
+            if n not in counts:
+                continue
+
+            while counts[n] > 0:
+                for i in range(groupSize):
+                    cur = n + i
+                    if counts[cur] == 0:
+                        return False
+                    counts[cur] -= 1
+        return True
+                
