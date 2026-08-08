c68827e31328d9c00df7e5f9597ef2742b30837b
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-08 19:01:13 -0400

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        chToI = {}
+        for i, c in enumerate(s):
+            if c not in chToI:
+                chToI[c] = [i]
+            else:
+                chToI[c].append(i)
+
+        print(chToI)
+        res = []
+        i = 0
+        while i < len(s):
+            ch = s[i]
+            nxt = chToI[ch][-1]
+            j = i
+            while j < nxt + 1:
+                if s[j] == ch:
+                    j += 1
+                    continue
+                nxt = max(nxt, chToI[s[j]][-1])
+                j += 1
+            print(nxt, i)
+            # print(chToI[ch][-1])
+            res.append(nxt - i + 1)
+            i = nxt + 1
+        return res
