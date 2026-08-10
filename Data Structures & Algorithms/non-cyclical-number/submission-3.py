5bf6c0413bb83719870af3981cc6f99feb201d64
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-10 11:00:57 -0400

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        fast = n
+        slow = n
+
+        def happy(n):
+            res = 0
+            while n:
+                res += (n % 10) ** 2
+                n = n // 10
+            return res
+        
+        while True:
+            slow = happy(slow)
+            fast = happy(happy(fast))
+            if fast == 1:
+                return True
+            if slow == fast:
+                return False
+        return True
