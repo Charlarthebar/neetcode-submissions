4c865b99dd47fb79fdca94870431165e07041a8b
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-10 20:39:47 -0400

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        pos = x >= 0
+        numList = []
+        x = abs(x)
+
+        while x:
+            numList.append(x % 10)
+            x //= 10
+
+        target = 2**31 - 1 if pos else 2**31
+        target = list(str(target))
+
+        if len(numList) == len(target):
+            for n1, n2 in zip(numList, target):
+                if n1 != int(n2):
+                    if int(n2) < n1:
+                        return 0
+                    else:
+                        break
+            return int("".join(map(str, numList))) if pos else int("".join(map(str, numList))) * -1
+        else:
+            return int("".join(map(str, numList))) if pos else int("".join(map(str, numList))) * -1
