24c6c9744c34ea8a838354bf72c15aba86400449
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-15 19:25:49 -0400

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        hmap = defaultdict(int)
+
+        for n in nums:
+            hmap[n] += 1
+            if len(hmap) <= 2:
+                continue
+            
+            newhmap = defaultdict(int)
+            for n, count in hmap.items():
+                hmap[n] -= 1
+                if count > 1:
+                    newhmap[n] = count - 1
+            hmap = newhmap
+        
+        res = []
+        for n in hmap:
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+        return res
+        
