b21ec627a76f4aa9a40ca5b737c0d31ba49b8898
Charles Lai <121214530+Charlarthebar@users.noreply.github.com>
2026-08-19 14:47:40 -0400

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        sums = defaultdict(int)
+        total = 0
+        res = 0
+        for n in nums:
+            total += n
+            if total == k:
+                res += 1
+            res += sums[total - k]
+            sums[total] += 1
+        return res
